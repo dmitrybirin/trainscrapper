3dced2be888e85bd5c05a77b9991ba8bf278632e
Dmitry Birin <dmitry.birin@gmail.com>
2016-08-26 10:00:12 +0300

    port to env

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = express();
 
 app.get('/api/task/gatherDataFor/:daysCount', taskController.gatherDataForDays)
 
-var port = 1234;
+var port = process.env.PORT || 1234;
 
 app.listen(port, function(){
     console.log(`I'm listening to the port ${port}`)
