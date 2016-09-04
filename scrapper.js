331d0e5f4a1477add7a0f3317487d0e33d503411
Dmitry Birin <dmitry.birin@gmail.com>
2016-09-04 23:56:51 +0300

    timeout to env var HORSEMAN_TIMEOUT

--- scrapper.js
+++ scrapper.js
@@ -10,7 +10,7 @@ var logger = require('./logger')
 var currentUrl = 'https://pass.rzd.ru/'
 
 var horsemanInit = () => {
-    return new Horseman({timeout:20000})    
+    return new Horseman({timeout:process.env.HORSEMAN_TIMEOUT})    
     .userAgent('Mozilla/5.0 (Windows NT 6.1; WOW64; rv:27.0) Gecko/20100101 Firefox/27.0')
     .on('consoleMessage', (msg) => logger.silly(msg))
     .on('error', (error) => logger.error(error))
@@ -55,7 +55,6 @@ var checkDate = function(realDate){
     return new Promise( function( resolve, reject ){
         return getDate()
         .then(function(val){
-                logger.info('val is', val)
                 if (!val.includes(realDate)){
                     logger.debug('date isn\'t right incrementing...')
                     return horseman.click('.box-form__datetime__arrow-right.j-right')
