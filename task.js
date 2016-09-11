277125af74e921062acd648576ae1e434a2a0d96
Dmitry Birin <dmitry.birin@gmail.com>
2016-09-11 23:17:57 +0300

    fix bug with the time slot

--- task.js
+++ task.js
@@ -70,9 +70,9 @@ function scrapingOneDirection(direction, directionSeriesCallback){
 
 function getTimeSlot(hour){
     if (hour>0 && hour<=4) return 'night'
-    if (hour>5 && hour<=8) return 'morning'
-    if (hour>9 && hour<=12) return 'afternoon'
-    if (hour>13 && hour<=16) return 'daytime'
-    if (hour>17 && hour<=20) return 'evening'
-    if (hour>21 && hour<=24) return 'late evening'
+    if (hour>4 && hour<=8) return 'morning'
+    if (hour>8 && hour<=12) return 'afternoon'
+    if (hour>12 && hour<=16) return 'daytime'
+    if (hour>16 && hour<=20) return 'evening'
+    if (hour>20 && hour<=24) return 'late evening'
 }
